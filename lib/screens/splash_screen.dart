caf9ee5acd2b6def153ac2c06e7f94d57e99511b
laibasohailchatha <laibasohail2401@gmail.com>
2026-05-23 13:45:44 +0500

    initial commit

---
+++ lib/screens/splash_screen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text(
+          'FitTrack',
+          style: TextStyle(
+            fontSize: 32,
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+          ),
+        ),
+      ),
+    );
+  }
+}
